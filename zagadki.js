25d5fa8aa8a492f320efb99f9c5ec48852f5772a
repcak3310 <66223196+repcak3310@users.noreply.github.com>
2021-05-16 23:37:21 +0200

    Add files via upload

---
+++ zagadki.js
@@ -0,0 +1,55 @@
+function z1()
+{
+     document.getElementById("z1").innerHTML="Jedna bogini dostała jabłko z koszykiem.";
+}
+
+function z2()
+    {
+        document.getElementById("z1").innerHTML="Tu znajdziesz odpowiedź";
+    } 
+
+
+
+function z3()
+{
+     document.getElementById("z3").innerHTML="7 - A L F A B E T";
+}
+    
+function z4()
+    {
+        document.getElementById("z3").innerHTML="Tu znajdziesz odpowiedź";
+    }   
+        
+
+
+function z5()
+{
+     document.getElementById("z5").innerHTML="Jutro.";
+}
+        
+function z6()
+    {
+        document.getElementById("z5").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+function z7()
+{
+     document.getElementById("z7").innerHTML="Mały, czerwony trójkąt";
+}
+
+function z8()
+    {
+        document.getElementById("z7").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+    function z9()
+{
+     document.getElementById("z9").innerHTML="Nie padało";
+}
+
+function z10()
+    {
+        document.getElementById("z9").innerHTML="Tu znajdziesz odpowiedź";
+    }
